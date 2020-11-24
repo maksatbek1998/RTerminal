8b412e1ac15ee991ecadb2cf3e329bce7df4054a
maksatbek1998 <maksat.140298.kg@gmail.com>
2020-11-24 12:03:43 +0600

    Maksat 12:03

---
+++ RTerminal/RTerminal/Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace RTerminal
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1.5);
+            timer.Tick += Time;
+            timer.Start();
+        }
+        public void Time(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ RTerminal/RTerminal/Zagruzka.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Threading;
+
+namespace RTerminal
+{
+    /// <summary>
+    /// Логика взаимодействия для Zagruzka.xaml
+    /// </summary>
+    public partial class Zagruzka : Window
+    {
+        public Zagruzka()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(3);
+            timer.Tick += Time;
+            timer.Start();
+        }
+        public void Time(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+    }
+}
